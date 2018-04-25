ad2763583a731690d1bef36a6a0103a0efc176fd
Hector Moreno <hector2_10@hotmail.com>
2018-04-25 08:55:44 -0500

    se cambio el generador de operadores

--- Mancala.lisp
+++ Mancala.lisp
@@ -8,6 +8,91 @@
 (defparameter *current-turn* 1) ;;1 player o 2 AI
 (defparameter *finish* nil)
 (defparameter *winner* 0)
+(defparameter *ai-move* nil)
+
+;; (evalstate '((0 0 0) ((1 1 1) (1 1 1) (1 1 1) (1 1 1) (1 1 1) (1 1 1) (1 1 1) (1 1 1) (1 1 1) (1 1 1) (1 1 1) (0 0 0)) (1 0 0)))
+
+(defun evalState (board)
+	"Evalua el estado"
+	(let ((evl 0)) 
+		(setf evl 
+			(+ (* 13 (loop for i in (third board)
+						sum (* i  5)) ) 
+				(loop for h in (second board)
+						for i from 6 to 11
+						sum (loop for j from 0 to 2
+								sum (+ (* (nth j h) 5) 1) into x
+								finally (return (* x (mod i 6) )) ))
+				(loop for h in (second board)
+						for i from 1 to 6
+						sum (loop for j from 0 to 2
+								sum (+ (* (nth j h) 5) 1) into x
+								finally (return (* x (- i) ) ) )) 
+					 ))
+					 evl ))
+
+(defun combinations (&rest lists)
+	(if (car lists)
+		(mapcan (lambda (inner-val)
+		        (mapcar (lambda (outer-val)
+		                  (cons outer-val
+		                        inner-val))
+		                (car lists)))
+		      (apply #'combinations (cdr lists)))
+		(list nil)))
+
+
+(defun getMarblesList (board hole) 
+	(loop for i from 0 to 2
+		with marbles = nil
+		do (loop for j from 0 below (nth i (nth hole (second board)))
+				do (setf marbles (cons i marbles) ) ) 
+		finally (return marbles) ))
+
+(defun cloneBoard (board)
+	"Copia un nuevo tablero"
+	(let ((result nil) )
+		(setf result 
+			(append result (list (loop for g in (first board)
+								collect g)) ))
+		(setf result (append result 
+							(list (loop for h in (second board)
+								collect (loop for p in h
+											collect p))) ))
+		(setf result 
+			(append result (list (loop for g in (third board)
+								collect g)) ))
+		result))
+
+(defun apply-AI-move (board hole movements)
+	(loop for mov in movements
+		;do (print (first mov))
+		if (=  (first mov) -2)
+			do (setf (nth (second mov) (third board)) (+ (nth (second mov) (third board)) 1) )
+		else 
+			do (setf (nth (second mov) (nth (first mov) (second board))) (+ (nth (second mov) (nth (first mov) (second board))) 1) )
+		do (setf (nth (second mov) (nth hole (second board))) 0) ) 
+	
+	board)
+
+(defun groupMovements (movements num)
+	"Divide una lista en [num] partes"
+	(loop for i from 0 below num
+		collect (subseq movements (* num i) (+ (* num i) num) ) ))
+
+(defun filterOperators (operators start)
+	(loop for op in operators 
+		with table = nil
+		do (setf table (make-hash-table))
+		when (and (= (first (first op)) start) (loop for mov in op
+					if (gethash (first mov) table)
+						do (return Nil)
+						do (setf (gethash (first mov) table) 1)
+					finally (return T) ))
+			collect op into filtered
+		finally (return filtered)))
+
+
 
 (defun isHoleEmpty? (board num)
 	"Dice si el hoyo esta vacio"
@@ -21,20 +106,32 @@
 	(push newelt (cdr (nthcdr index lst))) 
 	lst)
 
+(defun addGoalMov (lst search goal)
+	(let ((new-lst nil) (sIndex (loop for x in lst
+						for i from 0 to (length lst)
+						when (= x search)
+							do (return i))))
+		(cond ((= sIndex 0) 
+						(setf new-lst (append (list goal) lst))
+						(setf new-lst (subseq new-lst 0 (- (length new-lst) 1) )) )
+			((= sIndex (- (length new-lst) 1)) 
+				(setf new-lst (subseq new-lst 0 (- (length lst) 1) ))
+				(setf new-lst (append new-lst (list goal) )) )
+			(T (setf new-lst (subseq lst 0 sIndex ))
+				(setf new-lst (append new-lst (list goal) ))
+				(setf new-lst (append new-lst (subseq lst sIndex (- (length lst) 1)) ) )) )
+		new-lst ))
+
 (defun generateMoves (board hole turn)
 	"Genera una lista con posibles movimientos"
 	(let ((moves '()) (marbles (getMarbles board hole)) )
 			(loop for i from (+ hole 1) to (+ hole marbles )
 					collect (mod i 12) into c
 					finally (setf moves c)) 
-			(when (= turn 1)
-					(when (>= (+ hole marbles) 6)
-							(insert-after moves (abs (- (+ hole marbles) 6 1)) -1 )
-							(setf moves (reverse (rest (reverse moves)))) ))
-			(when (= turn 2)
-					(when (>= (+ hole marbles) 12)
-							(insert-after moves (abs (- (+ hole marbles) 12 1)) -2 )
-							(setf moves (reverse (rest (reverse moves)))) )) 
+			(when (and (= turn 1) (>= (+ hole marbles) 6))
+					(setf moves (addGoalMov moves 6 -1)) )
+			(when (and (= turn 2) (>= (+ hole marbles) 12) )
+					(setf moves (addGoalMov moves 0 -2)) ) 
 			moves ))
 
 ;(defun move (board from type to)
@@ -57,7 +154,10 @@
 			(nth 1 (nth hole (second board))) 
 			(nth 2 (nth hole (second board))))
 		(setf type (read))
-		(cond ((or (< type 0) (> type 2))
+		(cond ((not (numberp type)) 
+				(format t "Error ingrese un numero entero:~&")
+				(move board movements hole))
+			((or  (< type 0) (> type 2))
 				(format t "~&Opcion invalida~&")
 				(move board movements hole))
 				((<= (nth type (nth hole (second board))) 0) 
@@ -79,26 +179,108 @@
 
 (defun isGameOver? (board)
 	"revisa si ya se acabo el juego"
-	(loop for h in (nth 1 board)
-		with over = nil
-		do (loop for c in h 
-				do (print c)
-				if (= c 1)
-					do (return (setf over nil))
-				do (setf over T))
-		when (null over)
-			do (return over)
-		finally (return over) ))
+	(let ((over nil))
+		(setf over (or (loop for i from 0 to 5
+						always (isHoleEmpty? board i)) 
+						(loop for i from 6 to 11
+						always (isHoleEmpty? board i)))) ))
 
+(defun gencombinations (movements marbles)
+	(cond ((= (length movements) 1)
+		(loop for marble in marbles
+						collect (list (first movements) marble)))
+		(T (loop for res in (gencombinations (rest movements) marbles)
+			with nextres = '()
+			do (loop for marble in marbles
+						if (listp (first res))
+							collect (append res (list (list (first movements) marble))) into temp
+						else
+							collect (list res (list (first movements) marble)) into temp
+						finally (setf nextres (append nextres temp)))
+			finally (return nextres) ))
+		
+	)
+)
+
+(defun getDiffMarbles (board hole)
+	"Obtiene una lista con los diferentes tipos de canicas de [hole]"
+	(loop for i from 0 to 2
+		when (nth i (nth hole (second board)) )
+		collect i))
+
+(defun negamax-alfabeta (board depth alfa beta )
+	(cond ((or (isGameOver? board) (> depth *limit*) )
+		;(print (list (evalState board) board depth alfa beta))
+		 (evalState board) )
+		(T (let ((bestMov nil) 
+				(bestHole nil)
+				(bestValue most-negative-fixnum))
+			(loop for i downfrom 11 to 6
+				with movements = nil
+				with operators = nil
+				with value = nil
+				with num-mov = nil
+				with start-mov = nil
+				;when (isHoleEmpty? board i)
+				;do (format t "~A Esta vació ~A" i (getMarbles board i))
+				when (not (isHoleEmpty? board i))
+				do (setf movements (generateMoves board i 2)) and
+				do (setf num-mov (length movements)) and
+				do (setf start-mov (first movements)) and
+				do (setf operators (gencombinations movements (getDiffMarbles board i))) and
+				;do (print operators) and
+				;do (print (list "A" board movements i )) and (filteroperators (apply #'combinations (groupmovements (combinations (generatemoves *board* 11 2) (getMarblesList *board* 11)) 4)) -2)
+				;do (setf operators (combinations movements (getMarblesList board i) )) and
+				do (setf movements nil) and
+				;do (setf operators (groupMovements operators num-mov)) and
+				;do (setf operators (apply #'combinations operators)) and
+				;do (setf operators (filterOperators operators start-mov)) and
+				;when (>= (length operators) 120)
+					;do (print "operators debe ser 10") and
+					;do (setf operators (subseq operators 0 10))
+				;end and
+				;do (print (list board depth i " Operadores = " (length operators))) and
+				do (loop for op1 in operators
+						with new-state = nil
+						with op = nil
+						if (not (listp (first op1))) 
+							do (setf op (list op1) )
+						else 
+							do (setf op op1)
+						do (setf new-state (cloneBoard board))
+						do (apply-AI-move new-state i op)
+
+						;do (format t "~A" op)
+						;do (print (list new-state (+ depth 1) beta alfa bestValue))
+						do (setf value (negamax-alfabeta new-state (+ depth 1) (- beta) (- (max alfa bestValue)) ) )
+						do (setf new-state nil)
+						;do (format t "~A value = ~A" bestValue value)
+						do (setf value (- value))
+						when (> value bestValue)
+							;do (print (list "value" value)) and
+							do (setf bestValue value) and
+							do (setf bestMov op) and
+							do (setf bestHole i) and
+							when (>= bestValue beta )
+								;do (print operators) and
+								do (return T)
+							end )
+				do (setf operators nil)
+				finally (setf *ai-move* (list bestHole bestMov bestValue))(return value) )
+			))))
 
 
 (defun ask-box ()
 	"Pide que selecciones una casilla"
 	(format t "Selecciona una casilla (0 al 5):~&")
 	(setq selected-box (read))
-	(when (or (< selected-box 0) (> selected-box 5) (isHoleEmpty? *board* selected-box))
-		(format t "Error no contiene canicas:~&")
-		(ask-box))
+	(cond ((not (numberp selected-box)) 
+				(format t "Error ingrese un numero entero:~&")
+				(ask-box))
+		((or (< selected-box 0) (> selected-box 5) (isHoleEmpty? *board* selected-box))
+				(format t "Error no contiene canicas:~&")
+				(ask-box))
+		(T T))
 	selected-box)
 
 (defun human-turn ()
@@ -115,7 +297,15 @@
 
 (defun Agent-turn ()
 	"Movimiento de la Agente"
-	(setf *finish* T))
+	(display-board)
+	(setf *ai-move* nil)
+	(negamax-alfabeta *board* 0 most-negative-fixnum most-positive-fixnum )
+	(let ((operators *ai-move*))
+		(print operators)
+		(apply-AI-move *board* (first operators) (reverse (second operators)))
+		(when (not (= (first (first (second operators))) -2)) 
+				(setf *current-turn* 1))
+								 ))
 
 (defun gameloop () 
 	"El ciclo de juego"
@@ -123,7 +313,7 @@
 	(if (= *current-turn* 1)
 		(human-turn)
 		(Agent-turn))
-	;(setf *finish* (isGameOver? *board*))
+	(setf *finish* (isGameOver? *board*))
 	(when (not *finish*)
 			(gameloop) ))
 
